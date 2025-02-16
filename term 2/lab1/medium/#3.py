dbaf739e5694333310a217538512e2146def761f
SetToFail <90303338+SetToFail@users.noreply.github.com>
2025-02-16 14:07:12 +0300

    Update #3.py

--- term 2/lab1/medium/#3.py
+++ term 2/lab1/medium/#3.py
@@ -1,7 +1,7 @@
 import random
-n = int(input("Введите размер массива: "))
-k = int(input("Введите число, на которое проверяем кратность: "))
-arr = [random.randint(1, 100) for _ in range(n)]
-print("Сгенерированный массив:", arr)
-count = sum(1 for x in arr if x % k == 0)
-print(f"Количество элементов, кратных {k}: {count}")
+size = int(input("Введите размер массива: "))
+num = int(input("Введите число, для проверки кратности: "))
+arr = [random.randint(1, 100) for _ in range(size)]
+count = sum(1 for x in arr if x % num == 0)
+print(f"Сгенерированный массив: {arr}")
+print(f"Количество элементов, кратных {num}: {count}")
