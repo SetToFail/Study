72f6d1f8d8cf6ce5b4adeb275dbf910a5df3272c
SetToFail <tsibindanila@gmail@.com>
2025-04-22 16:33:20 +0300

    Backup OS can crush

--- term 2/lab4/#2/#2.py
+++ term 2/lab4/#2/#2.py
@@ -4,8 +4,50 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
-def mission_home():
-    return render_template('base.html', title='Заготовка')
+@app.route('/index/<title>')
+def index(title='Заготовка'):
+    return render_template('base.html', title=title)
+
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof)
+
+@app.route('/list_prof/<list_type>')
+def list_prof(list_type):
+    professions = [
+        "Инженер-исследователь",
+        "Пилот",
+        "Строитель",
+        "Экзобиолог",
+        "Врач",
+        "Инженер по терраформированию",
+        "Климатолог",
+        "Специалист по радиационной защите",
+        "Астрогеолог",
+        "Гляциолог",
+        "Инженер жизнеобеспечения",
+        "Метеоролог",
+        "Оператор марсохода",
+        "Киберинженер",
+        "Штурман",
+        "Пилот Дронов"
+    ]
+    return render_template('list_prof.html', list_type=list_type, professions=professions)
+
+@app.route('/answer')
+@app.route('/auto_answer')
+def answer():
+    data = {
+        'title': 'Анкета',
+        'surname': 'Watny',
+        'name': 'Mark',
+        'education': 'высшее',
+        'profession': 'штурман марсохода',
+        'sex': 'male',
+        'motivation': 'Всегда мечтал застрять на Марсе!',
+        'ready': 'True'
+    }
+    return render_template('auto_answer.html', **data)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
